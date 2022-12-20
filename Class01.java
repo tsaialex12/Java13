cc60b0158354c8737c1e932750bdd89d0ac32787
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-12-21 00:22:28 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,13 @@
+package hw_13;
+
+public class hw01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=12,den=0;
+		int ans=num/den;
+		System.out.print(ans);
+	}
+
+}
+
