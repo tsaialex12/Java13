635fc326bcb743457f2bf4dcbb19d29c73e087d5
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-12-26 17:24:32 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,15 @@
+
+public class Class02
+{
+    public static void main(String[] args) {
+        int num=12,den=0;
+        if (den==0){
+            System.out.println("除數為0");   
+        }
+        else{
+        int ans=num/den;
+        System.out.println(ans);
+        }
+        
+    }
+}
